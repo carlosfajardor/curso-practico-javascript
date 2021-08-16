6dd33896a81612f52761eaa9d666e064a31baa90
carlosfajardor <carlos.fajardo.co@gmail.com>
2021-08-15 21:27:02 -0500

    Pagina web que permite calcular el precio final ingresando precio original y el descuento.

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,19 @@ function calcularPrecioconDescuento(precio, descuento){
 	return precioConDescuento
 }
 
+function onClickButtonPriceDiscount() {
+	const inputPrice = document.getElementById("InputPrice");
+	const priceValue = inputPrice.value;
+
+	const inputDiscount = document.getElementById("InputDiscount");
+	const discountValue = inputDiscount.value;
+
+	const precioConDescuento = calcularPrecioconDescuento(priceValue,discountValue);
+
+	const resultP = document.getElementById("ResultP");
+	resultP.innerText ="El precio con descuento es: $ " + precioConDescuento;
+
+}
 
 
 // console.log({
