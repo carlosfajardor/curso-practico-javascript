2f7d189e92d7032219745d8a3f6ceaee0965791b
carlosfajardor <carlos.fajardo.co@gmail.com>
2021-09-08 19:42:15 -0500

    Calculadora V2 mediana salarios

--- analisis.js
+++ analisis.js
@@ -1,46 +1,49 @@
-const salariosCol = colombia.map(
-  function (personita) {
-		return personita.salary;
-  }
-);
-
-const SalariosColSorted = salariosCol.sort(
-	function(salaryA, salaryB) {
-		return salaryA - salaryB;
-	}
-);
-
+// Helpers
 function esPar(numerito) {
-	return (numerito % 2 === 0);
+  return (numerito % 2 === 0);
 }
 
 function calcularMediaAritmetica(lista) {
-	const sumaLista = lista.reduce(
-		function (valorAcumulado = 0, nuevoElemento) {
-			return valorAcumulado + nuevoElemento;
-		}
-	);
-
-	const promedioLista = sumaLista / lista.length;
-
-	return promedioLista
+  const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento) {
+      return valorAcumulado + nuevoElemento;
+    }
+  );
+  const promedioLista = sumaLista / lista.length;
+  return promedioLista;
 }
 
-function medianaSalarios(lista){
-	const mitad = parseInt(lista.lenght / 2);
+// Calculadora de mediana
+function medianaSalarios(lista) {
+  const mitad = parseInt(lista.length / 2);
 
-	if (esPar(lista.lenght)) {
-		const personitaMitad1 = lista[mitad - 1];
-		const personitaMitad2 = lista[mitad];
+  if (esPar(lista.length)) {
+    const personitaMitad1 = lista[mitad - 1];
+    const personitaMitad2 = lista[mitad];
 
-		const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
-		return mediana;
-	} else {
-		const personitaMitad = lista[mitad];
-		return personitaMitad;
-	}
+    const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
+    return mediana;
+  } else {
+    const personitaMitad = lista[mitad];
+    return personitaMitad;
+  }
 }
 
-console.log(
-	medianaSalarios(SalariosColSorted)
+// Mediana General
+const salariosCol = colombia.map(
+  function (personita) {
+    return personita.salary;
+  }
 );
+
+const salariosColSorted = salariosCol.sort(
+  function (salaryA, salaryB) {
+    return salaryA - salaryB;
+  }
+);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+console.log ({
+	medianaGeneralCol
+});
